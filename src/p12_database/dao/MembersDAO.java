3c87c95d1cc6a3965050781c11d47cafbefbcad2
admin <zxcvbnh036@naver.com>
2024-07-01 16:26:28 +0900

    init

--- src/p12_database/dao/MembersDAO.java
+++ src/p12_database/dao/MembersDAO.java
@@ -10,9 +10,20 @@ public class MembersDAO extends DAOSet {
     try {
       conn = connectDB();
       String sql = "select * from members where id=? and pass=? ";
+      pstmt = conn.prepareStatement(sql);
+      pstmt.setString(1, id);
+      pstmt.setString(2, pass);
+      rs = pstmt.executeQuery();
+      if (rs.next()) {
+        members = new Members(rs.getLong("mno"),
+            rs.getString("id"),rs.getString("pass"),
+            rs.getString("name"),rs.getString("mobile")
+        );
+      }
     } catch (SQLException e) {
       throw new RuntimeException(e);
     } finally {
+      closeDB();
     }
     return members;
   }
