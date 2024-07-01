5ccc311f36822d39500748bcd4c9b0b523d2c9fa
admin <zxcvbnh036@naver.com>
2024-07-01 16:42:52 +0900

    init

---
+++ src/p12_database/ui/FrmBasic.java
@@ -0,0 +1,20 @@
+package p12_database.ui;
+
+import javax.swing.*;
+
+public abstract class FrmBasic extends JFrame {
+  private int width, height;
+  public FrmBasic(String title, int width, int height) {
+    this.width = width;this.height = height;
+    init(); arrange(); inflate();
+  }
+
+  public abstract void init();
+  public abstract void arrange();
+  public void inflate() {
+    setSize(width, height);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLocationRelativeTo(this);
+    setVisible(true);
+  }
+}
--- src/p12_database/ui/FrmLogin.java
+++ src/p12_database/ui/FrmLogin.java
@@ -1,5 +1,32 @@
 package p12_database.ui;
 
-public class FrmLogin {
+import javax.swing.*;
 
+public class FrmLogin extends FrmBasic {
+  private JLabel lbLogin, lbId, lbPass;
+  private JTextField tfId;
+  private JPasswordField pfPass;
+  private JButton btnLogin, btnJoin, btnClose;
+
+  public FrmLogin(String title, int width, int height) {
+    super(title, width, height);
+  }
+
+  @Override
+  public void init() {
+
+  }
+
+  @Override
+  public void arrange() {
+
+  }
+
+  @Override
+  public void inflate() {
+    super.inflate();
+  }
 }
+
+//label로 login창 만들기
+
